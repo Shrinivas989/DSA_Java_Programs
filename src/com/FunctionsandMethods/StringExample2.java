45365b4f4d3f40d91a156ae59579273ca78f0bf4
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-02-16 22:46:46 +0530

    updated

---
+++ src/com/FunctionsandMethods/StringExample2.java
@@ -0,0 +1,14 @@
+package com.FunctionsandMethods;
+
+public class StringExample2 {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+
+    }
+    static String greet(){
+        String greeting ="How are you";
+        return greeting;
+
+    }
+}
